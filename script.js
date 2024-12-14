5e83433ffa2e9d3ac5fcdb1cd89f79fb08771222
Tsehay Goremes <118212771+Tseehay@users.noreply.github.com>
2024-12-14 11:10:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText", {
-  strings: [" Backend Developer "],
+  strings: [" Machine Learning Engineer "],
   loop: true,
   typeSpeed: 100,
   backSpeed: 80,
